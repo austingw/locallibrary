ab94ba24726b61da75f3709121d9a575e119c703
austingw <senortinman@gmail.com>
2023-01-03 00:37:32 -0500

    Add delete book page and functionality

--- controllers/bookController.js
+++ controllers/bookController.js
@@ -197,13 +197,69 @@ exports.book_create_post = [
 ];
 
 // Display book delete form on GET.
-exports.book_delete_get = (req, res) => {
-  res.send("NOT IMPLEMENTED: Book delete GET");
+exports.book_delete_get = (req, res, next) => {
+  async.parallel(
+    {
+      book(callback) {
+        Book.findById(req.params.id).exec(callback);
+      },
+      book_bookinstances(callback) {
+        BookInstance.find({ book: req.params.id }).exec(callback);
+      },
+    },
+    (err, results) => {
+      if (err) {
+        return next(err);
+      }
+      if (results.book == null) {
+        // No results.
+        res.redirect("/catalog/books");
+      }
+      // Successful, so render.
+      res.render("book_delete", {
+        title: "Delete Book",
+        book: results.book,
+        book_bookinstances: results.book_bookinstances,
+      });
+    }
+  );
 };
 
 // Handle book delete on POST.
-exports.book_delete_post = (req, res) => {
-  res.send("NOT IMPLEMENTED: Book delete POST");
+exports.book_delete_post = (req, res, next) => {
+  async.parallel(
+    {
+      book(callback) {
+        Book.findById(req.params.bookid).exec(callback);
+      },
+      book_bookinstances(callback) {
+        BookInstance.find({ book: req.params.bookid }).exec(callback);
+      },
+    },
+    (err, results) => {
+      if (err) {
+        return next(err);
+      }
+      // Success
+      if (results.book_bookinstances.length > 0) {
+        // Books has bookinstances. Render in same way as for GET route.
+        res.render("book_delete", {
+          title: "Delete Book",
+          genre: results.book,
+          book_bookinstances: results.book_bookinstances,
+        });
+        return;
+      }
+      // Book has no bookinstances. Delete object and redirect to the list of books.
+      Book.findByIdAndRemove(req.body.bookid, (err) => {
+        if (err) {
+          return next(err);
+        }
+        // Success - go to genre list
+        res.redirect("/catalog/books");
+      });
+    }
+  );
 };
 
 // Display book update form on GET.
